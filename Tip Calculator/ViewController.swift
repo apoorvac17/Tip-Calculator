c5df41e986f51f83104caaebe12e53e777666162
Apoorva Chintala <apoorva@wireless-10-148-254-132.public.utexas.edu>
2019-01-27 21:46:25 -0600

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Apoorva Chintala on 1/27/19.
+//  Copyright © 2019 Apoorva Chintala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
